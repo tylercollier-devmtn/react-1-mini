8250d5370c9370ed1bc832d8f08fdbc4834c42ff
Tyler Collier <tyler.collier+github@devmounta.in>
2018-06-22 11:52:10 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>lkjsdlkfjasdklfj</div>
+    );
+  }
+}
+
+export default App;
+
+export const myString = 'something';
